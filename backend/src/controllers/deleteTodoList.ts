311463f99b4367e2e9f82ae709ae669fb4308626
rmachaal <rahafam@hotmail.co.uk>
2024-06-23 17:43:46 +0100

    Amends deleteTodoList endpoint.

--- backend/src/controllers/deleteTodoList.ts
+++ backend/src/controllers/deleteTodoList.ts
@@ -1,20 +1,36 @@
 import { Request, Response } from "express";
 import prisma from "../models/prismaClient";
 
-// DELETE /todolists/:id - Delete a todo list
+// DELETE /todolists/:id - Delete a todo list and its items
 export const deleteTodoList = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-    const deletedTodoList = await prisma.todoList.delete({
+    const itemsToDelete = await prisma.item.findMany({
+      where: {
+        listId: parseInt(id),
+      },
+    });
+
+    await prisma.item.deleteMany({
+      where: {
+        listId: parseInt(id),
+      },
+    });
+
+    await prisma.todoList.delete({
       where: {
         id: parseInt(id),
       },
     });
 
-    res.json({ message: "Todo list deleted successfully" });
+    res.json({
+      message: "Todo list and associated items deleted successfully",
+    });
   } catch (error) {
-    console.error("Error deleting todo list:", error);
-    res.status(500).json({ error: "Error deleting todo list" });
+    console.error("Error deleting todo list and associated items:", error);
+    res
+      .status(500)
+      .json({ error: "Error deleting todo list and associated items" });
   }
 };
