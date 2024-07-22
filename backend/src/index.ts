0ed520e01b99e81b6f7f38e599fe412aa219c375
rmachaal <rahafam@hotmail.co.uk>
2024-07-22 20:22:07 +0100

    Updates Prisma schema to use CockroachDB.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import { deleteTodoList } from "./controllers/deleteTodoList";
 import { removeItemFromList } from "./controllers/removeItemFromList";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3300;
 
 app.use(cors());
 app.use(express.json());
