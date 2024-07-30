4a548b193b793eaca51e42d5ab0a83667a498647
rmachaal <rahafam@hotmail.co.uk>
2024-07-30 17:44:13 +0100

    Updates editTodoListItem controller.

--- backend/src/controllers/editTodoListItem.ts
+++ backend/src/controllers/editTodoListItem.ts
@@ -3,14 +3,21 @@ import prisma from "../models/prismaClient";
 
 export const editTodoListItem = async (req: Request, res: Response) => {
   const itemId = req.params.id;
+  const listId = req.params.listId;
+
   try {
     const updatedItem = await prisma.item.update({
-      where: { id: parseInt(itemId) },
+      where: {
+        id: parseInt(itemId),
+        listId: parseInt(listId),
+      },
       data: { completed: true },
     });
+
+    console.log(`Updated item ${itemId} in list ${listId}:`, updatedItem);
     res.json(updatedItem);
   } catch (err) {
-    console.error(err);
+    console.error("Error updating item:", err);
     res
       .status(500)
       .json({ error: "An error occurred while updating the item." });
